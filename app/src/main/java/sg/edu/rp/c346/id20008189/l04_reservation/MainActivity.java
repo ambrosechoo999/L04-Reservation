7facc8301363bb9452b548a4f93b98cb5bd510df
CHOO KAI WAN <20008189@myrp.edu.sg>
2021-05-17 14:51:10 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20008189/l04_reservation/MainActivity.java
@@ -0,0 +1,47 @@
+package sg.edu.rp.c346.id20008189.l04_reservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnSubmit;
+    Button btnReset;
+    TextView tvShow;
+    DatePicker dp;
+    TimePicker tp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tp= findViewById(R.id.tp);
+        btnSubmit=findViewById(R.id.setRes);
+        btnReset=findViewById(R.id.btnReset);
+        dp=findViewById(R.id.dp);
+        tvShow=findViewById(R.id.tvShow);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Reservation has been set!",Toast.LENGTH_LONG).show();
+                String date ="Reservation for "+dp.getDayOfMonth() + "/"+(dp.getMonth()+1)+"/"+dp.getYear() +" "+"\n"+ + tp.getCurrentHour()+":"+tp.getCurrentMinute();
+
+                tvShow.setText(date);
+            }
+
+        });
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Reservation has been reset!",Toast.LENGTH_LONG).show();
+                tvShow.setText("");
+            }
+        });
+
+    }
+}
